2e51752a2e593eb931d5cb6f3172aaa78f470471
bleemb <bleemb@gmail.com>
2021-04-29 22:26:16 +1000

    added support for JSON, handling roles w/ managed policies only, updated output functionality to create report

--- cfn_access_analyzer.py
+++ cfn_access_analyzer.py
@@ -12,8 +12,17 @@ sts_client = boto3.client('sts')
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("path", help="Path to the cfn template to analyse")
+    parser.add_argument("--path", action='store', type=file_path, required=True, help="Path to the cfn template to analyse")
+    parser.add_argument('--output', action='store', type=str, required=True, choices=['print', 'file'], help= 'Toggle printing output to cli, or writing to a file')
     return parser.parse_args()
+
+
+# validation on whether path to file is valid
+def file_path(string):
+    if os.path.isfile(string):
+        return string
+    else:
+        raise FileNotFoundError(string)
 
 
 def parse_cfn(policy_array, account_id, region):
@@ -54,8 +63,9 @@ def parse_cfn(policy_array, account_id, region):
 
 
 def validate_policy(policy_array):
+    results = {}
     for policy in policy_array:
-        print(f'Analysing: {policy["PolicyName"]}')
+        print(f'---Analysing: {policy["PolicyName"]}---')
         try:
             response = retry.api.retry_call(
                 access_client.validate_policy,
@@ -68,15 +78,29 @@ def validate_policy(policy_array):
             )
             findings = response['findings']
             if len(findings) > 0:
-                print(findings)
+                results.update({
+                    policy["PolicyName"]: ''
+                })
+                findings_array = []
+                for finding in findings: 
+                    findings_array.append({
+                        "Finding Code": f"{finding['issueCode']} ({finding['findingType']})",
+                        "Finding Details": finding['findingDetails'],
+                        "Learn more link": finding['learnMoreLink']
+                    })
+                
+                results.update({
+                    policy["PolicyName"]: findings_array
+                })
+                
             else:
                 print(f"No findings found for {policy['PolicyName']}")
-                print('\n')
         
         except access_client.exceptions.InternalServerException: 
             print(f"Failed to validate {policy['PolicyName']}")
             print('\n')
             next
+    return results
 
 
 if __name__ == '__main__':
@@ -87,26 +111,41 @@ if __name__ == '__main__':
     region = os.getenv("REGION")
     # To do: #Read in parameter file, and be able to parse in Ref functions based off it
     path = args.path
-    try:
-        with open(path, 'r') as document: 
-            cfn_template = json.loads(to_json(document.read()))
-            Resources = cfn_template['Resources']
-
-            policy_array = [{'PolicyName': policy, 'PolicyDocument': Resources[policy]['Properties']['PolicyDocument']} for policy
-                in Resources.keys() if Resources[policy]['Type'] == 'AWS::IAM::ManagedPolicy']
-            
-            for policy in Resources.keys():
-                if Resources[policy]['Type'] == 'AWS::IAM::Role':
-                    for Policy in Resources[policy]['Properties']['Policies']:
-                        
-                        policy_array.append({
-                            'PolicyName': Policy['PolicyName'],
-                            'PolicyDocument': Policy['PolicyDocument'],
-                        })
-            
-            parse_policy_array = parse_cfn(policy_array, account_id, region)
-            validate_policy(policy_array)
     
+    output = args.output
+    try:
+        extension = path.split('.')[-1]
+        with open(path, 'r') as document: 
+            if extension == 'yaml' or extension == 'yml':
+                cfn_template = json.loads(to_json(document.read()))
+                Resources = cfn_template['Resources']
+            else:
+                cfn_template = json.loads(document.read())
+                Resources = cfn_template['Resources']
     except FileNotFoundError as e:
         print(f"Error when trying to locate path.  Check Path and try again.")
         raise(e)
+
+    # Customer managed policies
+    policy_array = [{'PolicyName': policy, 'PolicyDocument': Resources[policy]['Properties']['PolicyDocument']} for policy
+        in Resources.keys() if Resources[policy]['Type'] == 'AWS::IAM::ManagedPolicy']
+
+    for policy in Resources.keys():
+        if Resources[policy]['Type'] == 'AWS::IAM::Role':
+            # Accounting for Roles that only have managed policies attached
+            if Resources[policy]['Properties'].get('Policies') != None: 
+                for Policy in Resources[policy]['Properties']['Policies']:
+                    policy_array.append({
+                        'PolicyName': Policy['PolicyName'],
+                        'PolicyDocument': Policy['PolicyDocument'],
+                    })
+        
+    parse_policy_array = parse_cfn(policy_array, account_id, region)
+    print('\n Validating Policies...\n')
+    results = validate_policy(policy_array)
+
+    if output == 'print':
+        print(json.dumps(results, indent=4))
+    else:
+        with open('results.json', 'w') as doc: 
+            json.dump(results, doc, indent=4)
